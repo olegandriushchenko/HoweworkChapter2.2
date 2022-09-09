206738106988465f1d14ab7e3c9fd73881102037
oleg_andriushchenko <olegandriushchenko95@gmail.com>
2022-09-09 14:06:20 -0500

    Homework

---
+++ HomeworkChapter2.2.py
@@ -0,0 +1,12 @@
+my_string='test automation'
+word_test=my_string[0:5]
+print(word_test)
+
+my_string = "Test Automation"
+second_word = my_string[5:15]
+print(second_word)
+
+my_list = [10,50,20,25,30]
+sliced_list = my_list[0:3]
+print(sliced_list)
+ 
